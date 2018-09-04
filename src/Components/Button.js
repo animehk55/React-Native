dd662f66451bc93b3bb18c4c8b52189ad64b960a
MPSILU <SOF@gmail.com>
2018-09-04 10:29:04 +0530

    not working as it will span the whole area

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -24,7 +24,7 @@ const styles = {
  
     buttonStyle: {
         flex: 1,
-        alignself: 'stretch',   
+        alignself: 'stretch',   //not working alignSelf as it will span the whole area 
         backgroundColor: '#fff',
         borderRadius: 5,
         borderWidth: 1,
