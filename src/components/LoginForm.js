650c286e0a9bd87fce69c140ec917dd489a374c6
MPSILU <SOF@gmail.com>
2018-09-04 23:25:39 +0530

    deleted for pull request

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,13 +18,7 @@ class LoginForm extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <TextInput
-                        secureTextEntry 
-                        placeholder="password"
-                        label="Password"
-                        value={this.state.password}
-                        onChangeText={password => this.setState({ password })}
-                    />
+                    
                 </CardSection>
                 
 
