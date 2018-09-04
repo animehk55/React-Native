bd22f34465e58c228ea87f19acea9fcf421b7175
MPSILU <SOF@gmail.com>
2018-09-04 10:00:53 +0530

    not working as it will span the whole area

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,14 +1,37 @@
-import React from 'react';
+    import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+    const { buttonStyle } = styles;
     return (
-        <TouchableOpacity >
-            <Text> 
+        <TouchableOpacity>
+            <Text style={buttonStyle}> 
                 Click me!!!
             </Text>
         </TouchableOpacity>
     );
 };
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontweight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+ 
+    buttonStyle: {
+        flex: 1,
+        alignself: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    }
+}
 
 export default Button;
