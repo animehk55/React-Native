e497d65ad9d063f5209af52bdbc2c74633dfde05
MPSILU <SOF@gmail.com>
2018-09-04 12:52:08 +0530

    removed props

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
