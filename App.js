6a61141f40753bc23b431957b58afb6566532a47
ANIMESh <smileonlyforeverani@gmail.com>
2018-09-06 04:14:45 +0530

    till form validation left

--- App.js
+++ App.js
@@ -3,19 +3,11 @@ import { View } from 'react-native';
 // import firebase from 'firebase';
 import { Header } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
+// import * as firebase from 'firebase';
 
 
 export default class App extends Component {
-  // componentWillMount() {
-  //   firebase.initializeApp({
-  //   apiKey: 'AIzaSyC8B88ZHO2T3jsDaubm6c5QcJEWW_tS3jc',
-  //   authDomain: 'authenticationnative.firebaseapp.com',
-  //   databaseURL: 'https://authenticationnative.firebaseio.com',
-  //   projectId: 'authenticationnative',
-  //   storageBucket: 'authenticationnative.appspot.com',
-  //   messagingSenderId: '2437047853'
-  // }); 
-  // }
+
   render() {
     return (
       <View>
