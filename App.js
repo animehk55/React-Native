b93ed6242ec64a5e90bbd27066ff45ffa7f61e4f
ANIMESh <smileonlyforeverani@gmail.com>
2018-09-06 02:19:36 +0530

    firebase importing error

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import { Header } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 
 export default class App extends Component {
-  componentWillMount() {
-    firebase.initializeApp({
-    apiKey: 'AIzaSyC8B88ZHO2T3jsDaubm6c5QcJEWW_tS3jc',
-    authDomain: 'authenticationnative.firebaseapp.com',
-    databaseURL: 'https://authenticationnative.firebaseio.com',
-    projectId: 'authenticationnative',
-    storageBucket: 'authenticationnative.appspot.com',
-    messagingSenderId: '2437047853'
-  }); 
-  }
+  // componentWillMount() {
+  //   firebase.initializeApp({
+  //   apiKey: 'AIzaSyC8B88ZHO2T3jsDaubm6c5QcJEWW_tS3jc',
+  //   authDomain: 'authenticationnative.firebaseapp.com',
+  //   databaseURL: 'https://authenticationnative.firebaseio.com',
+  //   projectId: 'authenticationnative',
+  //   storageBucket: 'authenticationnative.appspot.com',
+  //   messagingSenderId: '2437047853'
+  // }); 
+  // }
   render() {
     return (
       <View>
